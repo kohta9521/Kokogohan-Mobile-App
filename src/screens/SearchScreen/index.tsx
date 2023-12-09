f1e901fc3abb1e85aa0c1292cfbafe21863cf6d2
k_95.21 <kohta0421kochi@gmail.com>
2023-12-09 15:40:01 +0900

    add new search page inputtext

--- src/screens/SearchScreen/index.tsx
+++ src/screens/SearchScreen/index.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { Searchbar } from 'react-native-paper';
 
 
 const SearchScreen = () => {
+        // search
+        const [searchQuery, setSearchQuery] = React.useState('');
+
+        const onChangeSearch = query => setSearchQuery(query);
+    
     return (
         <View>
-            <Text>SearchScreen</Text>
+            <Searchbar
+                placeholder="ここごはんを検索"
+                onChangeText={onChangeSearch}
+                value={searchQuery}
+                style={styles.searchBar}
+                theme={{
+                    colors: {
+                        primary: 'red', // ボーダーの色
+                        text: 'white', // テキストの色
+                        placeholder: 'white', // プレースホルダーの色
+                        background: 'rgba(255, 255, 255, 0.3)', // 背景色 (白色に blur をかけるために透明度を指定)
+                    },
+                }}
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    searchBar: {
+        fontSize: 14,
+        backgroundColor: 'gray', // テーマカラーに合わせて変更
+        elevation: 10, // シャドウを削除
+    },
+    searchInput: {
+        color: 'red', // テキストの色を白に変更
+    },
+})
 
 export default SearchScreen;
