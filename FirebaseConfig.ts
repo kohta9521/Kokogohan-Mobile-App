71c6927f14fdce4576b256f6e697fb7d83f501f1
k_95.21 <kohta0421kochi@gmail.com>
2023-12-22 09:27:02 +0900

    api key delete

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-a0UV5mE9z2zvz-TrmfGSW_ZTV6PolE8",
-    authDomain: "kokogohan-app.firebaseapp.com",
-    projectId: "kokogohan-app",
-    storageBucket: "kokogohan-app.appspot.com",
-    messagingSenderId: "588351562490",
-    appId: "1:588351562490:web:e32cd0aaa9e803eb2adf43",
-    measurementId: "G-6TYVL2W43Z"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 
