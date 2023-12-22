daef1e907522ede2e9f3aae6e06ceb6ce7772d2f
k_95.21 <kohta0421kochi@gmail.com>
2023-12-22 10:21:17 +0900

    add all files reset

--- App.tsx
+++ App.tsx
@@ -1,102 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, Icon } from 'react-native';
-
-// react-native-navigation
-import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-// components import
-import HomeScreen from './src/screens/HomeScreen';
-import MapScreen from './src/screens/MapScreen';
-import SearchScreen from './src/screens/SearchScreen';
-import RankingScreen from './src/screens/RankingScreen';
-import MypageScreen from './src/screens/MypageScreen';
-import Logo from './src/components/atoms/Logo';
-
-// icons
-import { Ionicons } from '@expo/vector-icons';
-import { FontAwesome } from '@expo/vector-icons';
-
-
-const Tab = createBottomTabNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={() => ({
-          // header部分のスタイリング
-          headerStyle: {
-            backgroundColor: '#ff0211',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-          // tabBar部分のスタイリング
-          tabBarActiveTintColor: '#ff0211',
-          tabBarInactiveTintColor: 'gray',
-          // headerLeft部分の要素の記述
-          // headerLeft: () => (
-          //   <Logo />
-          // ),
-          // headerRight部分の要素の記述
-          // headerRight: () => (
-          //   <Button
-          //     onPress={() => alert('This is a button!')}
-          //     title='通知'
-          //   />
-          // ),
-        })}
-      >
-        <Tab.Screen
-          name="ホーム"
-          component={HomeScreen}
-          options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="ios-home" size={size} color={color} />
-              ),
-          }}
-        />
-        <Tab.Screen
-          name="見つける"
-          component={SearchScreen}
-          options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="search" size={size} color={color} />
-              ),
-          }}
-        />
-        <Tab.Screen
-          name="マップ"
-          component={MapScreen}
-          options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="map" size={size} color={color} />
-              ),
-          }}
-        />
-        <Tab.Screen
-          name="ランキング"
-          component={RankingScreen}
-          options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="trophy" size={size} color={color} />
-              ),
-          }}
-        />
-        <Tab.Screen
-          name="マイページ"
-          component={MypageScreen}
-          options={{
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="person" size={size} color={color} />
-              ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
